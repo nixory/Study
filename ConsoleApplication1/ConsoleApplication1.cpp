750e139126009436dade8a80f6d8331755fcdbc3
Nikita Stepanovs(nixory) <nikitastepanovs777@gmail.com>
2022-09-29 10:59:45 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>;
+using namespace std;
+
+int main() {
+	int a, b, res;
+	char op;
+	cin >> a >> b >> op;
+	bool f = true;
+	switch (op) {
+	case '+': res = a + b; break;
+	case '-': res = a - b; break;
+	case '*': res = a * b; break;
+	case '/': res = a / b; break;
+	default: cout << "Unknown operation";
+		f = false;
+	}
+	if (f) cout << "Result is: " << res;
+
+	
+
+	return 0;
+};
